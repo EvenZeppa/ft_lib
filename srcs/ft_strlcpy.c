56118532f9bc90318a25ff3926cc8f19b1af44d8
Even Zeppa <zeppaeven.dev@gmail.com>
2024-11-05 12:59:54 +0100

    UPD ft_strlcpy but not finished...

--- srcs/ft_strlcpy.c
+++ srcs/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ezeppa <ezeppa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 12:20:38 by ezeppa            #+#    #+#             */
-/*   Updated: 2024/11/05 12:21:16 by ezeppa           ###   ########.fr       */
+/*   Updated: 2024/11/05 12:56:40 by ezeppa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	
+	size_t	i;
+
+	i = 0;
+	while (i < size - 1)
+		dst[i] = src[i++];
+	dst[i] = '\0';
+	return (size - 1);
 }
