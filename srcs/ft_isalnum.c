0ab7bd3f781f052bf2398ba29be5f43ebc1869c3
Even Zeppa <zeppaeven.dev@gmail.com>
2024-11-05 10:09:42 +0100

    ADD ft_isalnum + start libft.h define and prototypes

--- srcs/ft_isalnum.c
+++ srcs/ft_isalnum.c
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ezeppa <ezeppa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/05 10:07:52 by ezeppa            #+#    #+#             */
-/*   Updated: 2024/11/05 10:08:35 by ezeppa           ###   ########.fr       */
+/*   Created: 2024/11/05 10:04:25 by ezeppa            #+#    #+#             */
+/*   Updated: 2024/11/05 10:09:27 by ezeppa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "../includes/libft.h"
 
-int	ft_isalpha(unsigned char c);
-int	ft_isdigit(unsigned char c);
-int	ft_isalnum(unsigned char c);
-
-#endif
+int	ft_isalnum(unsigned char c)
+{
+	if (ft_isalpha(c) || ft_isdigit(c))
+		return (1);
+	return (0);
+}
